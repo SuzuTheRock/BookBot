f24abc7a518a627bbebc8f673b6025dda924909e
Suzu <suzutherock@yahoo.com>
2024-03-15 03:12:49 -0400

    wrote the count function

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = read_the_text(book_path)
+#    print(type(text))
+    count(text)
+#    print(text)
+
+def read_the_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+    
+def count(text):
+    word_count = 0
+    words = text.split()
+    for word in words:
+        word_count += 1
+    print(word_count)
+
+
 if __name__ == "__main__":
     main()
